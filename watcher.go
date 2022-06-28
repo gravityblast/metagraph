d107d48ff87d924bcb9c046a48e715102fae007a
Andrea Franz <andrea@gravityblast.com>
2022-06-28 08:04:23 +0200

    parse embedded meta pointer

--- watcher.go
+++ watcher.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
-	"log"
+	"encoding/json"
+	"fmt"
+	"io"
 	"math/big"
+	"net/http"
+	"time"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
@@ -12,17 +16,27 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// FIXME: create a personal dedicated on pinata.cloud
+const IPFS_GATEWAY_URL = "https://ipfs.io/ipfs"
+const IPFS_CLIENT_TIMEOUT = 5
+
 type Watcher struct {
-	Client *ethclient.Client
-	Config *EventConfig
-	Logger *logrus.Entry
+	Client     *ethclient.Client
+	Config     *EventConfig
+	Logger     *logrus.Entry
+	ipfsClient *http.Client
 }
 
 func newWatcher(client *ethclient.Client, config *EventConfig) *Watcher {
+	ipfsClient := &http.Client{
+		Timeout: IPFS_CLIENT_TIMEOUT * time.Second,
+	}
+
 	return &Watcher{
-		Client: client,
-		Config: config,
-		Logger: logger.WithField("object", "watcher"),
+		Client:     client,
+		Config:     config,
+		Logger:     logger.WithField("object", "watcher"),
+		ipfsClient: ipfsClient,
 	}
 }
 
@@ -32,6 +46,7 @@ func (w *Watcher) Run() {
 		"action":  "run",
 		"runUUID": runUUID.String(),
 	})
+
 	addr := common.HexToAddress(w.Config.ContractAddress)
 	eventTopicId := w.Config.ABI.Events[w.Config.EventName].ID
 	fromBlock := big.NewInt(w.Config.StartingBlock)
@@ -60,7 +75,74 @@ func (w *Watcher) Run() {
 	for _, vLog := range logs {
 		ptr, err := parseEvent(w.Config, vLog.Data)
 		if err != nil {
-			log.Fatal(err)
+			l.WithFields(logrus.Fields{
+				"error": err.Error(),
+			}).Error("error parsing event")
+		}
+
+		l.WithFields(logrus.Fields{
+			"metadataProtocol": ptr.Protocol,
+			"metadataPointer":  ptr.Pointer,
+		}).Info("metaPointer found")
+
+		if len(w.Config.Embedded) > 0 {
+			w.parseEmbedded(runUUID, ptr)
+		}
+	}
+}
+
+func (w *Watcher) parseEmbedded(runUUID uuid.UUID, ptr *MetaPointer) {
+	l := w.Logger.WithFields(logrus.Fields{
+		"action":           "parseEmbedded",
+		"runUUID":          runUUID.String(),
+		"metadataProtocol": ptr.Protocol,
+		"metadataPointer":  ptr.Pointer,
+	})
+
+	l.Info("fetching")
+	u := fmt.Sprintf("%s/%s", IPFS_GATEWAY_URL, ptr.Pointer)
+
+	resp, err := w.ipfsClient.Get(u)
+	if err != nil {
+		l.WithFields(logrus.Fields{
+			"error": err.Error(),
+		}).Error("error fetching")
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		l.WithFields(logrus.Fields{
+			"status":     resp.Status,
+			"statusCode": resp.StatusCode,
+		}).Error("unexpected status code")
+		return
+	}
+
+	content, err := io.ReadAll(resp.Body)
+	if err != nil {
+		l.WithFields(logrus.Fields{
+			"error": err.Error(),
+		}).Error("error reading content")
+		return
+	}
+
+	var data map[string]interface{}
+	err = json.Unmarshal(content, &data)
+	if err != nil {
+		l.WithFields(logrus.Fields{
+			"error": err.Error(),
+		}).Error("error parsing json")
+		return
+	}
+
+	for _, embed := range w.Config.Embedded {
+		ptr, err := parseMetaPointerFromMap(embed, data)
+		if err != nil {
+			l.WithFields(logrus.Fields{
+				"error": err.Error(),
+			}).Error("error parsing event")
+			continue
 		}
 
 		l.WithFields(logrus.Fields{
